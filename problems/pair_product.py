241cd392c4a6a37097af9d13652a634b89f70818
Chris Lee <leechr2012@gmail.com>
2023-04-21 17:12:19 -0700

    pair sum

---
+++ problems/pair_product.py
@@ -0,0 +1,19 @@
+# Write a function, pair_product, that takes in a list and a target product as arguments.
+# The function should return a tuple containing a pair of indices whose elements multiply to the given target.
+# The indices returned must be unique.
+
+# Be sure to return the indices, not the elements themselves.
+
+# There is guaranteed to be one such pair whose product is the target.
+
+def pair_product(numbers, target_product):
+  map = {}
+  for index, num in enumerate(numbers):
+    complement = target_product / num
+    if complement in map:
+      return (map[complement], index)
+    map[num] = index
+
+#Time Complexity:
+# The time complexity for this O(n) as it will only have to iterate through the list once.
+#Inserting and lookup in the hashmap created will also be constant time O(n)
